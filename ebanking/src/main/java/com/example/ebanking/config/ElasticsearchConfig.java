585c542737425a4cb53c8bb5f4d3e54b5b921e22
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-12 15:32:21 +0100

    configue of connection with the elasticsearch

---
+++ ebanking/src/main/java/com/example/ebanking/config/ElasticsearchConfig.java
@@ -0,0 +1,26 @@
+package com.example.ebanking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.time.Duration;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.example.ebanking.repository.elasticsearch")
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl.replace("http://", ""))
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withSocketTimeout(Duration.ofSeconds(30))
+                .build();
+    }
+}
