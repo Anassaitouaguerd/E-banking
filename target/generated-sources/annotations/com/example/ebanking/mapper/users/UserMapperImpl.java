3480f01792d3fe2d2a82274a0daaef112447e4ba
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-11-12 15:36:17 +0100

    change the date

--- target/generated-sources/annotations/com/example/ebanking/mapper/users/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/ebanking/mapper/users/UserMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-03T21:40:50+0100",
+    date = "2024-11-11T22:34:07+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.2 (Oracle Corporation)"
 )
 @Component
