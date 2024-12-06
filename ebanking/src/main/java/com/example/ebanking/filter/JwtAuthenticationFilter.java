94394bc475d9da8910dcf90f93a9becc71aad00e
Anassaitouaguerd <aitouaguerdanass@gmail.com>
2024-12-06 21:09:22 +0100

    add logger just to debbuggin my userDetails class what he be returned

--- ebanking/src/main/java/com/example/ebanking/filter/JwtAuthenticationFilter.java
+++ ebanking/src/main/java/com/example/ebanking/filter/JwtAuthenticationFilter.java
@@ -75,6 +75,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                log.info("Load user details: {}", userDetails);
 
                 if (!jwtUtil.isTokenExpired(jwt)) {
                     UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
